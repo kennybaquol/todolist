f1178444ef3da762d1c4b9a1415a803b0772b027
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2024-09-24 20:06:43 -0400

    Add catch-all route to redirect to the login page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// @todo: Rip this out, as well as any views or controllers related to it
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -22,6 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::fallback(function () {
+    return redirect()->route('login');
 });
 
 require __DIR__.'/auth.php';
