8550d101dd14b7a9feead0d7c4ab816e9d207571
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2024-10-14 20:31:44 -0400

    Replace dashboard with home

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return Inertia::render('Home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
