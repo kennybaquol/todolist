d89b1cb16b63e24f0838b7fb6f1ae03358db8e9f
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2024-10-14 21:07:06 -0400

    Update feature test for unathenticated catch-all route

---
+++ tests/Feature/FallbackRouteTest.php
@@ -0,0 +1,12 @@
+<?php
+
+it('should redirect all unathenticated routes to the login page', function ($url) {
+    $response = $this->get($url);
+
+    $response->assertStatus(302);
+})->with([
+    ['/', 302],
+    ['/another-url', 302],
+    ['/home', 302],
+]);
+
