3b8635a61df11fbd2cc34a1757c48cd9b73276bc
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2024-11-25 20:07:18 -0500

    wip: add checkboxes and ability to add more

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,22 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const checkboxes = ref([{ value: 0, checked: false }, { value: 1, checked: false }]);
+
+const addCheckbox = async (index) => {
+    const newCheckbox = {
+        value: checkboxes.value.length + 1,
+        checked: false
+    };
+    
+    checkboxes.value.splice(index + 1, 0, newCheckbox);
+}
+
+const deleteCheckbox = (checkboxId) => {
+    // this needs to be written
+}
 </script>
 
 <template>
@@ -15,7 +31,24 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">
+                        <div
+                          v-for="(checkbox, index) in checkboxes" 
+                          :key="checkbox" 
+                          class="flex items-center space-x-2 mb-2"
+                        >
+                            <input type="checkbox" class="w-6 h-6 m-2" v-model="checkbox.checked" :id="checkbox.value" />
+                            <span class="w-full" :class="{ 'line-through': checkbox.checked }" :for="checkbox.value">
+                                <input
+                                    type="text"
+                                    class="w-full border-0 focus:ring-0"
+                                    :class="{ 'line-through': checkbox.checked }"
+                                    placeholder="edit me"
+                                    @keydown.enter.prevent="addCheckbox(index)"
+                                ></input>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
