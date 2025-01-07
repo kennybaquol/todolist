03296ca70afe5c89a672a9bde884dd9af6ebda24
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2025-01-06 19:40:41 -0500

    wip: additional DB config

--- config/database.php
+++ config/database.php
@@ -31,10 +31,10 @@ return [
 
     'connections' => [
 
-        'pgsql' => [
-            'driver' => 'pgsql',
+        'sqlite' => [
+            'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.pgsql')),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
@@ -82,9 +82,21 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+        'pgsql' => env('DATABASE_URL') ? [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
+            'host' => parse_url(env('DATABASE_URL'))['host'],
+            'port' => parse_url(env('DATABASE_URL'))['port'],
+            'database' => ltrim(parse_url(env('DATABASE_URL'))['path'], '/'),
+            'username' => parse_url(env('DATABASE_URL'))['user'],
+            'password' => parse_url(env('DATABASE_URL'))['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ] : [
+            'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
